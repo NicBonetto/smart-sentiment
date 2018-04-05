9aae425e06075caff27119a00096c23bf76c7d08
Nic Bonetto <nick.bonetto@gmail.com>
2018-04-05 16:38:52 -0700

    Declared getTestBatch function. #2

--- src/deep-learning/helper.py
+++ src/deep-learning/helper.py
@@ -26,7 +26,24 @@ def getTrainBatch():
       labels.append([1, 0])
     else:
       num = randint(13499, 24999)
+      labels.append([0, 1])
     
     arr[i] = ids[num-1:num]
 
+  return arr, labels
+
+def getTestBatch():
+  labels = []
+  arr = np.zeros([batchSize, maxSeqLength])
+
+  for i in range(batchSize):
+    num = randint(11499, 13499)
+
+    if (num <= 12499):
+      labels.append([1, 0])
+    else:
+      labels.append([0, 1])
+    
+    arr[i] = ids[num-1:num]
+  
   return arr, labels
